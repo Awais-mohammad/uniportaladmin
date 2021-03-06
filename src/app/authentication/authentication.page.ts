487b5c8bd098b0fbb600ccb4015779cf38748423
awaisdev <muhammadawais083@gmail.com>
2021-03-07 02:46:48 +0500

    setkrdoo

--- src/app/authentication/authentication.page.ts
+++ src/app/authentication/authentication.page.ts
@@ -1,13 +1,8 @@
+
+import { AngularFireAuth } from 'angularfire2/auth';
 import { Component, OnInit, HostListener } from '@angular/core';
 import { Router } from '@angular/router';
 
-interface admin {
-  name: string;
-  phone: number;
-  email: string;
-  password: string;
-
-}
 
 
 @Component({
@@ -21,11 +16,14 @@ export class AuthenticationPage implements OnInit {
 
   constructor(
     private router: Router,
+    private firebaseauth: AngularFireAuth,
 
   ) { }
 
   signup: boolean = false;
   width = window.innerWidth;
+  email: string;
+  password: string;
 
 
   @HostListener('window:resize', ['$event'])
@@ -38,7 +36,37 @@ export class AuthenticationPage implements OnInit {
   }
 
   gotoPage(pagename) {
-    this.router.navigate([pagename])
+    // this.router.navigate([pagename])
+  }
+
+  login() {
+    if (!this.email) {
+      alert('email cannot be left blank')
+    }
+    else if (!this.password) {
+      alert('password cannot be left blank')
+    }
+    else {
+      this.firebaseauth.auth.signInWithEmailAndPassword(this.email, this.password).then(() => {
+        alert('LOGGED IN SUCCESSFULL')
+      }).catch(err => {
+        alert(JSON.stringify(err.message))
+      })
+    }
+  }
+
+  resetPass() {
+    if (!this.email) {
+      alert('Enter account email to recover password')
+    }
+    else {
+      this.firebaseauth.auth.sendPasswordResetEmail(this.email).then(() => {
+        alert('EMAIL SENT ')
+      }).catch(err => {
+        alert(JSON.stringify(err.message))
+      })
+    }
+
   }
 
   ngOnInit() {
